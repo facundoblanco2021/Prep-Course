9f2b774cbaf0ab7853d49f3abab66fa61dcb09ee
facundoblanco2021 <norberto.blanco886@hotmail.com.ar>
2022-01-20 02:39:18 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza cnull` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = `hola`;
+const nuevaString = `holafacu`;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
